0fa796738e71e3fcde48ea2717727299bed896aa
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-09-11 11:05:23 +0800

    Update contoh4_13_ms173.py to start the test

--- contoh4_13_ms173.py
+++ contoh4_13_ms173.py
@@ -2,20 +2,16 @@
 dan nombor kecil di kanan"""
 
 def besar_kecil(x,y):   # x dan y ialah parameter, user-defined function ialah besar_kecil()
-    if x > y:           # struktur kawalan pilihan / dwipilihan if-else digunakan
-        return x, y
-    else:
-        return y, x
+    pass                # struktur kawalan pilihan / dwipilihan if-else digunakan
+        
     
 def main():
     #bahagian utama atur cara
     #Minta pengguna memasukkan dua nombor
-    a = int(input("Masukkan nombor integer yang pertama: "))    
-    b = int(input("Masukkan nombor integer yang kedua: "))      
+         
 
     #panggilan user-defined function
-    [besar, kecil] = besar_kecil(a, b)      
-    print(f"Nombor {besar} lebih besar daripada {kecil}")
+    
 
 if __name__ == "__main__":
     main()
