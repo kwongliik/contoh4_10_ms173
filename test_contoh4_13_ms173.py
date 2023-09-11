7cf826ae4035efb522c84feed54cc11aea179680
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-09-11 10:53:34 +0800

    upload files

---
+++ test_contoh4_13_ms173.py
@@ -0,0 +1,24 @@
+from contoh4_13_ms173 import *
+from tud_test_base import *
+import pytest
+
+# @pytest.fixture
+# def radius():
+#     radius = 5
+#     return radius
+
+@pytest.mark.parametrize("a,b", [(4,5), (6, 3), (9,15)])
+def test_besar_kecil(a,b):
+    [x,y] = besar_kecil(a,b)
+    assert x > y
+    
+def test_main():
+    set_keyboard_input([8,10])
+    main()
+    output = get_display_output()
+
+    assert output == [
+        "Masukkan nombor integer yang pertama: ",
+        "Masukkan nombor integer yang kedua: ",
+        "Nombor 10 lebih besar daripada 8"
+    ]
