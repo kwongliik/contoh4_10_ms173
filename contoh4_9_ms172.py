82467aee86ea97f0500f3bd0ac6e1c70e0428136
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-04 13:12:00 +0800

    Update contoh4_9_ms172.py soalan v2

--- contoh4_9_ms172.py
+++ contoh4_9_ms172.py
@@ -1,15 +1,15 @@
 #fungsi yang mengira kuasa dua
-def kuasadua(x):    # x ialah parameter, nama user-defined function ialah kuasadua()
-    pass
+... kuasadua(x):    # x ialah parameter, nama user-defined function ialah kuasadua()
+    ...... x * x
 
 #bahagian utama atur cara
 #minta pengguna memasukkan satu nombor
 def main():
-    #nom = int(input("Masukkan satu nombor integer: "))
+    nom = int(.....("Masukkan satu nombor integer: .....
 
     #panggilan fungsi
-    nom_kuasa = kuasadua(nom)  
-    print(f"Kuasa dua bagi {nom} ialah {nom_kuasa}")
+    nom_kuasa = ................  
+    print(f"Kuasa dua bagi ....... ialah ............")
 
 if __name__ == "__main__":
     main()
