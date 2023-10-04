f8d751b501d25f66cebdec335b951ef35ee9e195
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-04 11:56:59 +0800

    Update contoh4_10_ms173.py soalan v2

--- contoh4_10_ms173.py
+++ contoh4_10_ms173.py
@@ -1,17 +1,26 @@
-"""Test: User-defined function yang memulangkan nombor besar di kiri
-dan nombor kecil di kanan"""
+"""User-defined function yang memulangkan nombor besar di kiri
+dan nombor kecil di kanan
+Lengkapkan kod Python pada ruang kosong yang disediakan"""
 
-def besar_kecil(x,y):   # x dan y ialah parameter, user-defined function ialah besar_kecil()
-    pass                # struktur kawalan pilihan / dwipilihan if-else digunakan
+..... besar_kecil(x,y):   # x dan y ialah parameter, user-defined function ialah besar_kecil()
+    if x > y:
+        ...............
+    else:
+        ...............
         
     
 def main():
     #bahagian utama atur cara
     #Minta pengguna memasukkan dua nombor
+    a = int(.....("Masukkan nombor integer yang pertama: ".....
+    b = int(.....("Masukkan nombor integer yang kedua: ".....
          
-
     #panggilan user-defined function
-    
+    [besar, kecil] = .......................
+    print(...."Nombor ........ lebih besar daripada ........")
 
+############################################
+# Jangan ubah kod di bawah!!
+############################################
 if __name__ == "__main__":
     main()
