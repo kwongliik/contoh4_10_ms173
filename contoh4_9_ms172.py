6aa1ba216a4644bf5241c017244fbe34ea690f6b
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-04 15:09:42 +0800

    Update contoh4_9_ms172.py give hints

--- contoh4_9_ms172.py
+++ contoh4_9_ms172.py
@@ -1,15 +1,15 @@
 #Lengkapkan kod Python pada ruang kosong yang disediakan
-#fungsi yang mengira kuasa dua
-... kuasadua(x):    # x ialah parameter, nama user-defined function ialah kuasadua()
+#subatur cara function yang mengira kuasa dua
+... kuasadua(....):    # Guna x sebagai parameter
     ...... x * x
 
-#bahagian utama atur cara
-#minta pengguna memasukkan satu nombor
+#atur cara utama
+#minta pengguna memasukkan satu nombor integer
 def main():
     nom = int(.....("Masukkan satu nombor integer: .....
 
     #panggilan fungsi
-    nom_kuasa = ................  
+    nom_kuasa = ................  # Guna nom sebagai argumen apabila memanggil subatur cara kuasadua() 
     print(f"Kuasa dua bagi ....... ialah ............")
 
 if __name__ == "__main__":
