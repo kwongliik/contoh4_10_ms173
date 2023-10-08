e5dc977dc27ed51b9f42656a510fd590821f801b
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-08 23:04:39 +0800

    Update contoh4_9_ms172.py 3

--- contoh4_9_ms172.py
+++ contoh4_9_ms172.py
@@ -1,16 +1,16 @@
 #Lengkapkan kod Python pada ruang kosong yang disediakan
 #subatur cara function yang mengira kuasa dua
-1..... kuasadua(2......):    # Guna x sebagai parameter
-    3....... x * x
+..... kuasadua(......):    # Guna x sebagai parameter
+    ....... x * x
 
 #atur cara utama
 #minta pengguna memasukkan satu nombor integer
 def main():
-    nom = int(4........("Masukkan satu nombor integer: "))
+    nom = int(........("Masukkan satu nombor integer: "))
 
     #panggilan fungsi
-    nom_kuasa = 5................  # Guna nom sebagai argumen apabila memanggil subatur cara kuasadua() 
-    print(f"Kuasa dua bagi 6......... ialah 7............")
+    nom_kuasa = ................  # Guna nom sebagai argumen apabila memanggil subatur cara kuasadua() 
+    print(f"Kuasa dua bagi ......... ialah ............")
 
 ##################################
 # Jangan ubah kod di bawah!!
