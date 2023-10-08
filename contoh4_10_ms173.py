4b228259ea9a39fe2568e626193fd81d652d9e9d
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-08 23:03:49 +0800

    Update contoh4_10_ms173.py 3

--- contoh4_10_ms173.py
+++ contoh4_10_ms173.py
@@ -1,8 +1,7 @@
-"""
-Lengkapkan kod Python pada ruang kosong yang disediakan"""
+#Lengkapkan kod Python pada ruang kosong yang disediakan
 
 # subatur cara function yang memulangkan nombor besar di kiri dan nombor kecil di kanan
-... besar_kecil(........):   # Guna x dan y sebagai parameter
+.... besar_kecil(........):   # Guna x dan y sebagai parameter
     if x > y:
         ...............
     else:
@@ -11,12 +10,12 @@ Lengkapkan kod Python pada ruang kosong yang disediakan"""
 # Atur cara utama    
 def main():
     #Minta pengguna memasukkan dua nombor integer
-    a = int(.....("Masukkan nombor integer yang pertama: ".....
-    b = int(.....("Masukkan nombor integer yang kedua: ".....
+    a = .....(.....("Masukkan nombor integer yang pertama: "))
+    b = int(input("Masukkan nombor integer yang kedua: "))
          
     #memanggil subatur cara function besar-kecil()
     [besar, kecil] = ....................... # Guna a dan b sebagai argumen
-    print(...."Nombor ........ lebih besar daripada ........")
+    print(f"Nombor ........ lebih besar daripada {kecil}")
 
 ############################################
 # Jangan ubah kod di bawah!!
